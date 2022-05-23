ecf6681bd5b3342af2dcfa9eee7b7daa7004e389
HaoranGeng <2000012975@stu.pku.edu.cn>
2022-05-23 20:53:58 +0800

    new

--- PlantSeedClassification/model.py
+++ PlantSeedClassification/model.py
@@ -1,5 +1,25 @@
 import torch.nn as nn
 from utils import *
+import functools
+class MLP(nn.Sequential):
+    def __init__(self, in_channels, out_channels, norm_fn=None, num_layers=2):
+        modules = []
+        for _ in range(num_layers - 1):
+            modules.append(nn.Linear(in_channels, in_channels))
+            if norm_fn:
+                modules.append(norm_fn(in_channels))
+            modules.append(nn.ReLU())
+        modules.append(nn.Linear(in_channels, out_channels))
+        return super().__init__(*modules)
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                nn.init.constant_(m.bias, 0)
+        nn.init.normal_(self[-1].weight, 0, 0.01)
+        nn.init.constant_(self[-1].bias, 0)
+
 class ConvNet_noNorm(nn.Module):
     def __init__(self, num_class = LABEL_NUM):
         super(ConvNet_noNorm,self).__init__()
@@ -26,6 +46,7 @@ class ConvNet_noNorm(nn.Module):
 class ConvNet(nn.Module):
     def __init__(self, num_class = LABEL_NUM):
         super(ConvNet,self).__init__()
+        norm_fn = functools.partial(nn.BatchNorm1d, eps=1e-4, momentum=0.1)
         self.convlayer = nn.Sequential(
             nn.Conv2d(3, 8, 3),
             # nn.BatchNorm2d(8),
@@ -36,7 +57,6 @@ class ConvNet(nn.Module):
             nn.MaxPool2d(2),
             
             nn.Conv2d(8, 16, 3),
-            # nn.BatchNorm2d(16),
             nn.ReLU(inplace=True),
             nn.Conv2d(16, 16, 3),
             nn.BatchNorm2d(16),
@@ -45,7 +65,7 @@ class ConvNet(nn.Module):
             nn.Flatten(),
             nn.Linear(15625*16, 128),
             nn.Dropout(p=0.5),
-            # nn.BatchNorm1d(1)
+            norm_fn(128),
             nn.ReLU(inplace=True),
             nn.Linear(128, num_class)
         )
